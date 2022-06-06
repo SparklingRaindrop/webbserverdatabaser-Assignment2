c6f171a067676488349e43db7c0c2dc201c4776e
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-06-06 22:54:02 +0200

    Add function handleLog

--- js/logWriter.js
+++ js/logWriter.js
@@ -1,6 +1,34 @@
+const onFinished = require('on-finished');
+const { writeFile } = require('fs/promises');
+
+const DATE_OPTIONS = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+};
+
+const LOG_FILE = './server.log';
+
 function logWriter() {
-    return function log(req, res, next) {
-        console.log('\x1b[36m%s\x1b[0m', 'I am cyan');
+    // Getting time when the request comes in
+    const date = new Date().toLocaleDateString(undefined, DATE_OPTIONS);
+
+    return function handleLog(req, res, next) {
+        const method = req.method;
+        const route = req.url;
+
+        onFinished(res, async (err, res) => {
+            const logData = `[${date}] "${method} ${route}" ${res.statusCode}\n`;
+
+            writeFile(LOG_FILE, logData, {
+                encoding: 'utf8',
+                flag: 'a',
+            });
+        });
+
         next();
     }
 }
