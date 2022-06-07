9ca0529af859b5476251580b748409bf93f05475
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-06-07 16:51:02 +0200

    Add userStatement & re-form

--- config/database.js
+++ config/database.js
@@ -5,21 +5,33 @@ const db = new sqlite3.Database('./config/db.sqlite', (error) => {
         console.error(error.message);
         throw error;
     }
-    const statement = `
-        CREATE TABLE IF NOT EXISTS books (
+    const bookStatement = `
+        CREATE TABLE IF NOT EXISTS Book (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT,
             isbn TEXT,
             author TEXT,
             publishYear INTEGER,
             publisher TEXT
-        )
+        );
     `;
 
-    db.run(statement, (error) => {
-        if (error) {
-            console.error(error.message);
-        }
+    const userStatement = `
+        CREATE TABLE IF NOT EXISTS User (
+            id TEXT PRIMARY KEY,
+            name TEXT,
+            family_name TEXT,
+            email TEXT UNIQUE,
+            password TEXT
+        );
+    `;
+
+    [bookStatement, userStatement].forEach(statement => {
+        db.run(statement, (error) => {
+            if (error) {
+                console.error(error.message);
+            }
+        });
     })
 });
 
