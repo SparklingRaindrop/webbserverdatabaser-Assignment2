526d0f52f03c6ce6b77a322e16551d02e84c72dd
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-06-07 19:21:35 +0200

    Add token checker middleware

---
+++ middlewares/tokenChecker.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+function verifyToken(req, res, next) {
+    const token = req.headers.authorization;
+    if (!token) {
+        res.status(403).send('Token required');
+        return;
+    }
+    const [_, jwtToken] = token.split(' ');
+    try {
+        const decoded = jwt.verify(jwtToken, process.env.SECRET_KEY);
+    } catch (error) {
+        res.status(400).send('Invalid token.');
+        return;
+    }
+    next();
+}
+
+module.exports = verifyToken;
