00ad90c4f7e23d42b44b6f3555ab56c27c1d0dd8
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-06-07 21:15:09 +0200

    Initialize /users/ router

---
+++ routers/users.router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const controller = require('../controllers/users.controller');
+const tokenChecker = require('../middlewares/tokenChecker');
+
+const router = express.Router();
+
+router.post('/lend', tokenChecker, controller.borrowBook);
+router.post('/return',tokenChecker, controller.returnBook);
+
+module.exports = router;
