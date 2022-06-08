ac44ea307cf0fd98114b2ffaae630e17daaebdc8
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-06-08 17:20:37 +0200

    Add property user to req

--- middlewares/tokenChecker.js
+++ middlewares/tokenChecker.js
@@ -9,6 +9,7 @@ function verifyToken(req, res, next) {
     const [_, jwtToken] = token.split(' ');
     try {
         const decoded = jwt.verify(jwtToken, process.env.SECRET_KEY);
+        req.user = decoded;
     } catch (error) {
         res.status(400).send('Invalid token.');
         return;
