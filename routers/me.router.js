0c1e0bf18b5a97750e9a972e1b26394af2b1921a
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-06-12 19:43:12 +0200

    Change endpoint

--- routers/me.router.js
+++ routers/me.router.js
@@ -4,6 +4,6 @@ const controller = require('../controllers/me.controller');
 const tokenChecker = require('../middlewares/tokenChecker');
 
 const router = express.Router();
-router.post('/', tokenChecker, controller.getUserData);
+router.get('/', tokenChecker, controller.getUserData);
 
 module.exports = router;
